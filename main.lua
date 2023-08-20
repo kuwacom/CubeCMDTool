a5cdefbf6f532cb24eaa6c27315120b73fb81019
kuwacom <kuwa.com3@gmail.com>
2023-08-20 19:27:16 +0900

    1.3
    kickAll を追加
    OP以外のすべてのプレイヤーをキック可能

--- main.lua
+++ main.lua
@@ -16,9 +16,10 @@ function Initialize(Plugin)
 
 	cPluginManager:BindCommand("/listEntity","CubeCMDTool.killEntity",HandleListEntityCommand,"list all dimension entity")
 	
-	cPluginManager:BindCommand("/playerViewDistance","CubeCMDTool.",HandlePlayerViewDistanceCommand,"change player view distance")
-	cPluginManager:BindCommand("/allPlayerViewDistance","CubeCMDTool.",HandleAllPlayerViewDistanceCommand,"change all player view distance")
+	cPluginManager:BindCommand("/playerViewDistance","CubeCMDTool.playerViewDistance",HandlePlayerViewDistanceCommand,"change player view distance")
+	cPluginManager:BindCommand("/allPlayerViewDistance","CubeCMDTool.allPlayerViewDistance",HandleAllPlayerViewDistanceCommand,"change all player view distance")
 	
+	cPluginManager:BindCommand("/kickAll","CubeCMDTool.kickAll",HandleKiclAllCommand,"kick all player")
 	-- cPluginManager:AddHook(cPluginManager.HOOK_CHAT, Chat)
 
 	-- PLUGIN = Plugin -- NOTE: only needed if you want OnDisable() to use GetName() or something like that
@@ -139,5 +140,30 @@ function HandleAllPlayerViewDistanceCommand(Split, Player)
 	cRoot:Get():ForEachPlayer(ForEachPlayer)
 
 	Player:SendMessageSuccess("&eSet all player View distance to &a"..ViewDistance)
+	return true
+end
+
+
+function HandleKiclAllCommand(Split, Player)
+	local reason = ""
+	if not (Split[2] == nil) then
+		reason = Split[2]
+	end
+	local ForEachPlayer = function(_Player)
+		if not (_Player:GetName() == Player:GetName()) then
+			local haveAll = false
+			local permissions = _Player:GetPermissions()
+			for i = 1, #permissions do
+				if permissions[i] == "*" then
+					haveAll = true
+				end
+			end
+			if not haveAll then
+				cPluginManager:ExecuteConsoleCommand("kick ".._Player:GetName().." "..reason)
+			end
+		end
+	end
+	cRoot:Get():ForEachPlayer(ForEachPlayer)
+	Player:SendMessageSuccess("&eKick All Player")
 	return true
 end
